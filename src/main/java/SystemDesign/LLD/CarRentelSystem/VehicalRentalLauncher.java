58333901b55068a34d9e18513f9953ca3110e68c
kartikeyagg <kartikeyagg@gmail.com>
2025-01-21 23:48:33 +0530

    Finalising main

--- src/main/java/SystemDesign/LLD/CarRentelSystem/VehicalRentalLauncher.java
+++ src/main/java/SystemDesign/LLD/CarRentelSystem/VehicalRentalLauncher.java
@@ -8,7 +8,17 @@ public class VehicalRentalLauncher {
 		// TODO Auto-generated method stub
 		
 		VehicleRentalSystem vs = new VehicleRentalSystem() ;
-		System.out.println("book Vehicle ? select city");
+		
+		Vehicle v1 =  new Vehicle(0, "DL54E6353", 5600, Status.Active ,10) ;
+		Vehicle v2 = new Vehicle(1, "HR4Y0943", 300, Status.Active, 25) ;
+		Vehicle v3 = new Vehicle(2, "RJ3C4063", 1900, Status.Active, 20) ;
+		
+		
+		
+		
+//		Store s1 = new Store(0, null, null) 
+		
+		System.out.println("book Vehicle ? select city"); 
 		HashSet<String> cities =  vs.getCities() ;
 		for(String s: cities) {
 			System.out.println(s);
--- src/main/java/SystemDesign/LLD/CarRentelSystem/Vehicle.java
+++ src/main/java/SystemDesign/LLD/CarRentelSystem/Vehicle.java
@@ -4,17 +4,19 @@ public class Vehicle {
 	
 	private int id;
 	private String VehicleNumber;
-	private int kmDriver;
+	private int kmDriven;
 	private Status status;
+	private int costPerkm; 
 	
-	public Vehicle(int id, String vehicleNumber, int kmDriver, Status status) {
+	public Vehicle(int id, String vehicleNumber, int kmDriver, Status status , int costPerKm) {
 		this.id = id;
 		VehicleNumber = vehicleNumber;
-		this.kmDriver = kmDriver;
+		this.kmDriven = kmDriver;
 		this.status = status;
+		this.costPerkm =  costPerKm;
 	}
 
-	public int getId() {
+	public int getId() { 
 		return id;
 	}
 
@@ -22,13 +24,19 @@ public class Vehicle {
 		return VehicleNumber;
 	}
 
-	public int getKmDriver() {
-		return kmDriver;
+	public int getKmDriven() {
+		return kmDriven;
 	}
 
 	public Status getStatus() {
 		return status;
 	}
+
+	public int getCostPerkm() {
+		return costPerkm;
+	}
+	
+	
 	
 }
 
